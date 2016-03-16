9ff95369f025b89ddf46dd1796b0521a1f3153b9
Afroze Khan <auk2@njit.edu>
2016-03-16 17:02:19 -0400

    added bill editing

--- public/components/budget/controllers/budget.controller.js
+++ public/components/budget/controllers/budget.controller.js
@@ -11,8 +11,36 @@
 		console.log('loaded budgetCtrl');
 	
 
-		 $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  			$scope.data = [300, 500, 100];
+		 $scope.labels = ["Bills", "Budget", "Remaining"];
+  			$scope.data = [300, 500, 200];
+
+
+  			// income
+  			$scope.income = {}
+  			$scope.income.monthly = 4500;
+
+  			// bills
+  			$scope.bills =[]
+  			$scope.bills = [
+  				{name:"rent", cost: 2500},
+  				{name:"utilities", cost: 200},
+  				{name:"car insurance", cost: 150},
+  				{name:"car payment", cost: 250},
+  				{name:"gas", cost: 100},
+  				{name:"gym membership", cost: 50},
+  				{name:"cell phone", cost: 80},
+
+  			]
+
+  			$scope.addNewBill = function(){
+  				$scope.bills.push({name: $scope.newBillName, cost: 0 })
+  				$scope.newBillName = "";
+  			}
+
+  			$scope.removeBillItem = function(index){
+  				$scope.bills.splice(index,1);
+  			}
+
 
 
 
